7641727e252bdae27970e9524ffd0572cb223d9a
Feng Ya <fengya04@126.com>
2017-12-04 12:38:56 +0800

    webview

--- native/HomeScreen.js
+++ native/HomeScreen.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { WebView } from 'react-native';
+
+function template(body) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style type="text/css">
+      body {
+        color: black;
+      }
+      .mask {
+        background: black;
+      }
+      .highlight {
+        background: yellow;
+      }
+    </style>
+  </head>
+  
+  <body>
+    ${body}
+  </body>
+  
+  </html>
+  `;
+}
+
+const text = `
+Ein ganz neues Design aus Glas. Die beliebteste Kamera der Welt, jetzt noch besser. Der leistungs­stärkste und intelligenteste Chip, den es je in einem Smartphone gab. Kabelloses Laden – ganz einfach. Und Augmented Reality, so beeindruckend wie noch nie. iPhone 8. Eine neue iPhone Generation.
+`;
 
 export default class HomeScreen extends React.Component {
   render() {
-    return (
-      <View>
-        <Text>Home Screen</Text>
-      </View>
-    );
+    return <WebView source={{ html: template(text) }} />;
   }
 }
